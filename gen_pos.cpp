06b8ba193b405994527c269d87059add1612bd9a
bill-hscc <phongbinh2511@gmail.com>
2022-06-07 15:51:39 +0800

    [master] added gen pos.

---
+++ gen_pos.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int EMBB;
+int URLLC;
+int TIME_SLOT;
+int TIME_MINISLOT;
+
+default_random_engine g_positionEngine; // same seed all execution
+
+void getConfig() {
+  ifstream file;
+  file.open("config.txt");
+  file >> EMBB >> URLLC >> TIME_SLOT >> TIME_MINISLOT;
+  file.close();
+}
+
+string getMatlabFormat(int x, int y) {
+  return to_string(x) + "," + to_string(y);
+}
+
+void genEmbb() {
+  ofstream file;
+  file.open("./data/embb.txt");
+  uniform_int_distribution<int> uniform(0, 1000);
+  for (int i = 0; i < EMBB; ++i) {
+    file << getMatlabFormat(uniform(g_positionEngine), uniform(g_positionEngine)) << endl;
+  }
+  file.close();
+}
+
+string getUrllcFile(int timeSlot, int timeMinislot) {
+  return "./data/urllc-" + to_string(timeSlot) + "-" + to_string(timeMinislot) + ".txt";
+}
+
+void genUrllc(int timeSlot, int timeMinislot) {
+  ofstream file;
+  file.open(getUrllcFile(timeSlot, timeMinislot));
+  uniform_int_distribution<int> uniform(0, 1000);
+  for (int i = 0; i < URLLC; ++i) {
+    file << getMatlabFormat(uniform(g_positionEngine), uniform(g_positionEngine)) << endl;
+  }
+  file.close();
+}
+
+int main() {
+  getConfig();
+  genEmbb();
+  genUrllc(0, 0);
+  genUrllc(TIME_SLOT - 1, TIME_MINISLOT - 1);
+}
